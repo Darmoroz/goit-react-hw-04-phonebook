54f374e8672da787b2a85bcf22426c09846f13c6
Olexandr Darmoroz <darmoroz99@gmail.com>
2023-02-14 11:20:00 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ export class App extends Component {
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Larry Copeland', number: '287-91-26' },
     ],
     filter: '',
   };
